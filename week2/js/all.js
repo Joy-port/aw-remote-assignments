67ad90b803aff945249e3a81ccc3803638ef15dc
Joy Cheng <joy.cheng97324@gmail.com>
2022-01-10 20:53:24 +0800

    ignore DS_Store file

--- week2/js/all.js
+++ week2/js/all.js
@@ -102,4 +102,21 @@ function showMoreContent (e) {
     }
 }
 
-//assignment 4
+//assignment 5
+/* Batch #15 / Front-End Class
+Remote Learning Assignment - Week 2
+Assignment 5: Algorithm Practice (Advanced Optional)
+Given an array of integers, return indices of the two numbers such that they add up to a
+specific target. You may assume that each input would have exactly one solution, and you
+may not use the same element twice.
+function twoSum(nums, target){
+// your code here
+}
+/*
+For example:
+twoSum([2, 7, 11, 15], 9);
+Should returns:
+[0, 1]
+Because:
+nums[0]+nums[1] is 9
+* */
