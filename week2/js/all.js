39d996e809b4bb72c550a68ec49064be457434d2
Joy Cheng <joy.cheng97324@gmail.com>
2022-01-10 14:40:50 +0800

    finish assignment 4

--- week2/js/all.js
+++ week2/js/all.js
@@ -25,9 +25,9 @@ calculate(classObj.opErr2);
 //assignment 3
 avg(productData);
 
+
+
 //assignment 4
-
-
 //request 1
 const title = document.querySelector('#js-title');
 let times = 0 ;
@@ -46,11 +46,60 @@ function changeTitleContent(e){
 //request 2
 const menuBtn = document.querySelector('.navbar-button');
 const closeMenuBtn = document.querySelector('.mobile-menu-closeBtn');
+
+//display
+const menuCanvas = document.querySelector('.js-menu');
+const menuContent = document.querySelector('.mobile-menu-content');
+let isOpen = false;
 menuBtn.addEventListener('click',toggleMenu);
 closeMenuBtn.addEventListener('click', toggleMenu);
 
 function toggleMenu (e){
-    console.log(e.target.closest('button'));
+    if(e.target.closest('button')){
+        isOpen = !isOpen ;
+        showMenu(isOpen);
+    };
+    console.log(isOpen);
 }
 
-//assignment 5
+function showMenu(status){
+    if(status){
+        menuCanvas.style.display = 'block';
+        setTimeout(function() {
+            menuCanvas.classList.add('mobile-menu-showUp');
+            menuContent.classList.add('mobile-menu-slideIn');
+          }, 100);
+        
+    } else {
+        menuContent.classList.remove('mobile-menu-slideIn');
+        menuCanvas.classList.remove('mobile-menu-showUp');
+        menuCanvas.style.display = 'none';
+    }
+}
+
+//request 3
+const boxBtn = document.querySelector('#js-boxShowBtn');
+const boxContent = document.querySelectorAll('[data-box]');
+let clickNum = 0;
+boxBtn.addEventListener('click', showMoreContent);
+
+function showMoreContent (e) {
+    e.preventDefault();
+    if(e.target.nodeName !== "A"){
+       return;
+    };
+    clickNum ++ ;
+    if(boxContent[clickNum-1]){
+        boxContent[clickNum-1].style.display = 'flex'
+        if(clickNum === boxContent.length){
+            boxBtn.classList.add('disabled');
+            setTimeout(function() {
+            alert('All content are shown');
+        }, 100);
+        }
+    }else{
+        console.log('there is no more content');
+    }
+}
+
+//assignment 4
