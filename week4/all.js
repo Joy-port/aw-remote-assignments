fb19526cf3e786d457917b665d637ae846ab40f9
Joy Cheng <joy.cheng97324@gmail.com>
2022-01-25 11:43:40 +0800

    checked week2 hw

---
+++ week4/all.js
@@ -0,0 +1,29 @@
+//assignment 1
+
+function delayedResult(n1, n2, delayTime, callback){
+    const result = n1 + n2;
+    return setTimeout( callback(result), delayTime )
+}
+// delayedResult(4, 5, 3000, function(result){
+//     console.log(result);
+// }); 
+// delayedResult(-5, 10, 2000, function(result){
+//     window.alert(result);
+// });
+
+//assignment 2
+let data = [] ;
+const cors = 'https://cors-anywhere.herokuapp.com/';
+const url ='https://appworks-school.github.io/Remote-Aassigiment-Data/products';
+function ajax(src){
+
+}
+
+function render(data){
+// your code here.
+// document.createElement() and appendChild() are preferred. No innerHTML or // something alike
+
+} 
+
+    
+ // you should get product information in JSON format and render data in the page
