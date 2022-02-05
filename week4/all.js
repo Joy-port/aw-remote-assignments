c573b7435b9f88ba8fba516feecb666103259098
Joy Cheng <joy.cheng97324@gmail.com>
2022-02-05 13:55:22 +0800

    edit assignment 1

--- week4/all.js
+++ week4/all.js
@@ -2,16 +2,16 @@
 
 function delayedResult(n1, n2, delayTime, callback){
     const result = n1 + n2;
-    return setTimeout( callback(result), delayTime )
+    return setTimeout( () => callback(result), delayTime )
 }
 
 delayedResult(4, 5, 3000, function(result){
     console.log(result);
 }); 
 
-delayedResult(-5, 10, 2000, function(result){
-    window.alert(result);
-});
+// delayedResult(-5, 10, 2000, function(result){
+//     window.alert(result);
+// });
 
 //assignment 2
 let data = [] ;
